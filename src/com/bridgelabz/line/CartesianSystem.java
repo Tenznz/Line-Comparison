881d04e82ad8b82794af03ea86a7c484388e56fe
Duk <dhugkar@gmail.com>
2021-10-30 01:29:43 +0530

    added calculate length of line

--- src/com/bridgelabz/line/CartesianSystem.java
+++ src/com/bridgelabz/line/CartesianSystem.java
@@ -4,9 +4,22 @@ import java.util.Scanner;
 
 public class CartesianSystem {
 
+	public static void Line(int x1,int x2,int y1,int y2 ) {
+		    double line;
+		    line=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));	 	    
+	            System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+") is "+line);
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation Program on Master Branch"); 
+            int x1,x2,y1,y2;
+            System.out.println("Enter x1,y1 and x2,y2");
+            Scanner sc = new Scanner(System.in);
+            x1=sc.nextInt();
+            y1=sc.nextInt();
+            x2=sc.nextInt();
+            y2=sc.nextInt();
+            Line(x1,x2,y1,y2);
+            
 
 	}
-
 }
