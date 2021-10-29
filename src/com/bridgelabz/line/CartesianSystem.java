268f6f513233ae8255bcc83238f807ac65a9f16d
Duk <dhugkar@gmail.com>
2021-10-30 00:56:44 +0530

    Added welcome message

---
+++ src/com/bridgelabz/line/CartesianSystem.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.line;
+
+import java.util.Scanner;
+
+public class CartesianSystem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program on Master Branch"); 
+
+	}
+
+}
