79463c5c0e3f198c5907d0878cec350a59508421
Duk <dhugkar@gmail.com>
2021-11-08 20:40:43 +0530

    Modified: compareTo method

--- src/com/bridgelabz/line/CartesianSystem.java
+++ src/com/bridgelabz/line/CartesianSystem.java
@@ -7,7 +7,8 @@ public class CartesianSystem {
 	Scanner sc = new Scanner(System.in);
 
 	public double Line() {
-		System.out.println("Enter a line" + (count++) + " points (x1,y1) and (x2,y2)");
+
+		System.out.println("Enter a Line" + (count++) + " points (x1,y1) and (x2,y2)");
 		x1 = sc.nextInt();
 		y1 = sc.nextInt();
 		x2 = sc.nextInt();
@@ -15,23 +16,27 @@ public class CartesianSystem {
 		return ((double) Math.round(Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) * 100)) / 100;
 	}
 
-	private static void checkGreater(double line1, double line2) {
-		if (line1 > line2)
-			System.out.println("Line1 is greater than Line2");
-		else if (line1 < line2)
-			System.out.println("Line2 is greater than Line1");
-		else
-			System.out.println("Line1 and line2 are equal");
+	private static String compareTo(double line1, double line2) {
+		String msg = "";
+		int check = Double.compare(line1, line2);
+		if (check == 0) {
+			msg = "Line1 = Line2";
+		} else if (check > 0) {
+			msg = "Line1 > Line2";
+		} else
+			msg = "Line1 < Line2";
+		return msg;
 	}
 
 	public static void main(String[] args) {
 		double line1, line2;
 		CartesianSystem line = new CartesianSystem();
 		line1 = line.Line();
-		System.out.println("distancebetween" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") = " + line1);
+		System.out.println("Distance between" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") = " + line1);
 		line2 = line.Line();
-		System.out.println("distancebetween" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") = " + line2);
-		checkGreater(line1, line2);
+		System.out.println("Distance between" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") = " + line2);
+		String result = compareTo(line1, line2);
+		System.out.println(result);
 	}
 
 }
